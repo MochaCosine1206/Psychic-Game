098da3f358a4c5c8b9864484374dff253adce9f5
StevenXPS13 <steven.parrott@gmail.com>
2018-11-04 12:50:24 -0700

    html and js initial set up

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+let wins = 0;
+let losses = 0;
+let guesses = 9;
+let soFar = [];
+
+let emptyString = "";
+let alphabet = "abcdefhijklmnopqrstuvwxyz";
+
+while (emptyString.length < 6) {
+    emptyString += alphabet[Math.floor(Math.random)() * alphabet.length)];
+}
+
+
+
+console.log(emptyString);
