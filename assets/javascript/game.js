ad800205484e43e9b3e431ef13660e0d149f44fa
StevenXPS13 <steven.parrott@gmail.com>
2018-11-04 18:22:02 -0700

    testing of functions and output

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,27 @@ let wins = 0;
 let losses = 0;
 let guesses = 9;
 let soFar = [];
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+var emptyString = "";
 
-let emptyString = "";
-let alphabet = "abcdefhijklmnopqrstuvwxyz";
-
-while (emptyString.length < 6) {
-    emptyString += alphabet[Math.floor(Math.random)() * alphabet.length)];
+while (emptyString.length < 1) {
+    emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+let guess = function () {
+    if (emptyString === guess) {
+        wins = wins++
+    } else {
+        losses = losses++
+        guesses = guesses--
+    }
 }
 
 
 
+
+
 console.log(emptyString);
+console.log(guess("c"));
+console.log(losses);
+console.log(wins);
+console.log(guesses);
